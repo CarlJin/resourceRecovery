df1df9c481253ce061790a78d2ac3a441f22ccda
carl <982361920@qq.com>
2016-04-15 23:16:12 +0800

    首次提交

---
+++ src/test/java/com/lili/aaa.java
@@ -0,0 +1,18 @@
+package com.lili;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.Test;
+
+
+public class aaa {
+	 private final static Logger logger = LoggerFactory.getLogger(aaa.class); 
+
+	
+	@Test
+	public void resourceTest(){
+		logger.info("nihao");
+	}
+
+}
